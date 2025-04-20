f7a1a667659fd727a89d4f3ee4e8b77b4d2c5d20
Menglong Yang <menglongy2001@163.com>
2025-04-19 23:45:21 -0400

    update

--- code/render.R
+++ code/render.R
@@ -5,7 +5,7 @@ here::i_am(
 knitr::opts_knit$set(root.dir = here::here()) 
 
 rmarkdown::render(
-  here::here("code/index.Rmd"),
+  here::here("code/flexdashboard.Rmd"),
   output_dir = here::here("docs")
 )
 
